3515d275f16b393dea9b5884c35c62e04d1c04e1
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-04-25 17:58:11 +1000

    Initial Commit

---
+++ FairTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FairTask
+//
+//  Created by Na Tian on 25/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
