f247a792c4a5d6a7b7017a656e4b5288f8fbea2c
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-05-03 20:48:53 +1000

    version 1 project page - able to update total member count, edit member and task

--- FairTask/Control/NewProjectController.swift
+++ FairTask/Control/NewProjectController.swift
@@ -12,16 +12,20 @@ class NewProjectController: UIViewController, UITableViewDataSource, UITableView
 
     @IBOutlet weak var memberTable: UITableView!
     @IBOutlet weak var taskTable: UITableView!
+    @IBOutlet weak var totalMemberLabel: UILabel!
     
     var selectedProject: Project = Project(projectName: "", members: [], tasks: [])
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        totalMemberLabel.text = "Total: \(selectedProject.members.count)"
+        
         memberTable.register(AddMemberTableViewCell.nib(), forCellReuseIdentifier: AddMemberTableViewCell.identifier)
         memberTable.dataSource = self
         memberTable.delegate = self
         
+        taskTable.register(AddMemberTableViewCell.nib(), forCellReuseIdentifier: AddMemberTableViewCell.identifier)
         taskTable.dataSource = self
         taskTable.delegate = self
     }
@@ -33,9 +37,9 @@ class NewProjectController: UIViewController, UITableViewDataSource, UITableView
             // display the "Add member" cell if no members in the list, else display members and "Add member" cell
             numberOfRow = selectedProject.members.isEmpty ? 1 : selectedProject.members.count + 1
         case taskTable:
-            numberOfRow = selectedProject.tasks.count
+            numberOfRow = selectedProject.tasks.isEmpty ? 1 : selectedProject.tasks.count + 1
         default:
-            print("Some things wrong...")
+            break
         }
         return numberOfRow
     }
@@ -44,8 +48,8 @@ class NewProjectController: UIViewController, UITableViewDataSource, UITableView
         switch tableView {
         case memberTable:
             if indexPath.row >= selectedProject.members.count {
-                // Display add cell
-                let addCell = tableView.dequeueReusableCell(withIdentifier: "AddMemberTableViewCell", for: indexPath) as! AddMemberTableViewCell
+                // Display add member cell
+                let addCell = tableView.dequeueReusableCell(withIdentifier: AddMemberTableViewCell.identifier, for: indexPath) as! AddMemberTableViewCell
                 addCell.delegate = self // set the delegate to the view controller
                 cell = addCell
             } else {
@@ -55,13 +59,70 @@ class NewProjectController: UIViewController, UITableViewDataSource, UITableView
                 cell = listMemberCell
             }
         case taskTable:
-            let listMemberCell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
-            listMemberCell.textLabel?.text = selectedProject.tasks[indexPath.row].taskName
-            cell = listMemberCell
+            if indexPath.row >= selectedProject.tasks.count {
+                // Display add task cell
+                let addCell = tableView.dequeueReusableCell(withIdentifier: AddMemberTableViewCell.identifier, for: indexPath) as! AddMemberTableViewCell
+                addCell.delegate = self // set the delegate to the view controller
+                addCell.newMemberTextField.placeholder = "Add new task"
+                cell = addCell
+            } else {
+                let listTaskCell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+                let taskWeight = selectedProject.tasks[indexPath.row].taskWeight != nil ? "\(selectedProject.tasks[indexPath.row].taskWeight!)" : ""
+                listTaskCell.textLabel?.text = selectedProject.tasks[indexPath.row].taskName + " - " + taskWeight + "%"
+                cell = listTaskCell
+            }
         default:
             break
         }
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch tableView {
+        case memberTable:
+            if indexPath.row < selectedProject.members.count {
+                // Display an alert to edit the member text
+                let alertController = UIAlertController(title: "Edit Member", message: nil, preferredStyle: .alert)
+                alertController.addTextField { (textField) in
+                    textField.placeholder = "Enter member name"
+                    textField.text = self.selectedProject.members[indexPath.row]
+                }
+                alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                alertController.addAction(UIAlertAction(title: "Save", style: .default, handler: { (action) in
+                    // Update the member text and reload the table
+                    let newMemberName = alertController.textFields?.first?.text ?? ""
+                    self.selectedProject.members[indexPath.row] = newMemberName
+                    self.saveUpdatedProject()
+                    self.memberTable.reloadData()
+                }))
+                present(alertController, animated: true, completion: nil)
+            }
+        case taskTable:
+            if indexPath.row < selectedProject.tasks.count {
+                // Display an alert to edit the task text
+                let alertController = UIAlertController(title: "Edit Task", message: nil, preferredStyle: .alert)
+                alertController.addTextField { (textField) in
+                    textField.placeholder = "Enter task name"
+                    textField.text = self.selectedProject.tasks[indexPath.row].taskName
+                }
+                alertController.addTextField { (textField) in
+                    textField.placeholder = "Enter task weight"
+                    let taskWeight = self.selectedProject.tasks[indexPath.row].taskWeight
+                    textField.text = taskWeight != nil ? "\(taskWeight!)" : ""
+                }
+                alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                alertController.addAction(UIAlertAction(title: "Save", style: .default, handler: { (action) in
+                    // Update the task text and reload the table
+                    self.selectedProject.tasks[indexPath.row].taskName = alertController.textFields?.first?.text ?? ""
+                    self.selectedProject.tasks[indexPath.row].taskWeight = Int(alertController.textFields?.last?.text ?? "") ?? nil
+                    self.saveUpdatedProject()
+                    self.taskTable.reloadData()
+                }))
+                present(alertController, animated: true, completion: nil)
+            }
+        default:
+            break
+        }
     }
     
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
@@ -96,5 +157,6 @@ class NewProjectController: UIViewController, UITableViewDataSource, UITableView
             // Save the encoded data to UserDefaults
             UserDefaults.standard.set(encoded, forKey: PROJECT_KEY)
         }
+        totalMemberLabel.text = "Total: \(selectedProject.members.count)"
     }
 }
